6fcebda9e30fd8f4021be543707d419402cbfc9e
Rafael Sampaio Tavares <rafael_killua@msn.com>
2018-11-16 19:16:50 -0300

    Mostrando SSR!

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,26 @@
         <div>
             <h1>Paguru Nuxt</h1>
             <h2>I Paguru Summit Workshop: Apps with Nuxt.js</h2>
+            <h3>{{mensagem}}</h3>
         </div>
     </section>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        mensagem: "";
+    },
+
+    asyncData() {
+        if (process.server)
+            return {
+                mensagem: "Olá server!"
+            };
+        else
+            return {
+                mensagem: "Olá cliente!"
+            };
+    }
+};
 </script>
